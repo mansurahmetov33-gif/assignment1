6d6d5394224fea11070c8faa86bb86c10bdd61db
userp <mansurahmetov3@gmail.com>
2025-12-18 21:55:21 +0500

    the "Students" class has been created, where there are private fields, conditions for updating GPAs and issuing Honor

---
+++ src/Student.java
@@ -0,0 +1,67 @@
+public class Student {
+    private String name;
+    private String id;
+    private String major;
+    private double gpa;
+    private int credits;
+
+    public Student(String name, String id, String major){
+        this.name = name;
+        this.id = id;
+        this.major = major;
+        this.gpa = 0.0;
+        this.credits = 0;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCredits() {
+        return credits;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void updateGPA(double newGPA){
+        if(newGPA >= 0.0 && newGPA <= 4.0) {
+            this.gpa = newGPA;
+        }
+    }
+
+    public void addCredits(int c) {
+        if (c > 0){
+            this.credits += c;
+        }
+    }
+
+    public boolean isHonors(){
+        return gpa >= 3.5;
+    }
+
+
+}
